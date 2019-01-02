d8c626698e6be413750a75773c37d6d73599affa
Tank <fuch@fullsample.com>
2019-01-02 14:28:13 +0800

    [fuchun] #Tech add some config location

--- canalClient/src/main/java/com/tank/CanalClient.java
+++ canalClient/src/main/java/com/tank/CanalClient.java
@@ -2,15 +2,20 @@ package com.tank;
 
 import com.tank.memory.CanalExtractor;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author fuchun
  */
 public class CanalClient {
   public static void main(String[] args) {
     final CanalExtractor canalExtractor = new CanalExtractor("192.168.0.112", "demo2");
-    new Thread(canalExtractor, "Canal Extractor Thread").start();
+    final ExecutorService service = Executors.newSingleThreadExecutor();
+    service.execute(canalExtractor);
     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
       canalExtractor.stop();
+      service.shutdown();
       System.out.println("...over...");
     }));
   }
